cd306c06a8ca56f10a44ad50986e9e1a5a133327
hlzhu <zhuhuilingchanchu@163.com>
2017-04-03 01:33:54 -0400

    Test1.c

--- FileProject/src/Test1.c
+++ FileProject/src/Test1.c
@@ -9,12 +9,17 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *fp = fopen("", "r");
+	FILE *fp = fopen("/home/zhuhuiling/1.txt", "r");
 
 	if(fp == NULL)
 	{
 		printf("open file failed!\n");
 		return 0;
+	}
+	else
+	{
+		printf("open file success!\n");
+		fclose(fp);
 	}
 
 	return 0;
